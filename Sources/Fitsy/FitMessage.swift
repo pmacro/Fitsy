f3584b4fcc4f7d6c3b8a3eb748c0d73d26079548
Paul MacRory <pmacro@icloud.com>
2019-11-24 00:17:52 -0800

    Support the 'totals' message.

--- Sources/Fitsy/FitMessage.swift
+++ Sources/Fitsy/FitMessage.swift
@@ -245,6 +245,8 @@ extension FitFile {
       messageType = RecordMessage.self
     case .event:
       messageType = EventMessage.self
+    case .totals:
+      messageType = TotalsMessage.self
     default:
       print("Skipping message type \(messageNumber ?? .invalid) as Fitsy has not implemented this type.")
       return DummyMessage(messageType: messageNumber ?? .invalid,
@@ -559,6 +561,49 @@ public struct EventMessage: FitMessage {
 
     self.size = fields.totalFieldSize
   }
+}
+
+public struct TotalsMessage: FitMessage {
+  public var size: Int
+  
+  public var timestamp: Date!
+  public var timerTime: UInt32?
+  public var distance: UInt32?
+  public var calories: UInt32?
+  public var elapsedTime: UInt32?
+  public var activeTime: UInt32?
+  
+  public init?(data: Data, bytePosition: Int, fields: [MessageDefinition.Field]) {
+    var offset = bytePosition
+    
+    for field in fields {
+
+      switch field.number {
+      case 253:
+        guard let timestampInt = data[offset...].to(type: UInt32.self) else { return nil }
+        self.timestamp = Date(timeIntervalSinceReferenceDate: TimeInterval(timestampInt))
+      case 0:
+        self.timerTime = data[offset...].to(type: UInt32.self)
+      case 1:
+        self.distance = data[offset...].to(type: UInt32.self)
+      case 2:
+        self.calories = data[offset...].to(type: UInt32.self)
+      case 4:
+        self.elapsedTime = data[offset...].to(type: UInt32.self)
+      case 6:
+        self.activeTime = data[offset...].to(type: UInt32.self)
+      default:
+        break
+      }
+    
+      offset += Int(field.size)
+    }
+
+    self.size = fields.totalFieldSize
+
+  }
+  
+  
 }
 
 struct DeviceInfoMessage {
